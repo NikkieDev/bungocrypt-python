232a8125b321058877ef5f262f3d61324f738330
Nikkie Schaad <Nikkieschaad@gmail.com>
2022-07-10 18:36:38 +0200

    Commit

---
+++ crypt/encrypt.py
@@ -0,0 +1,14 @@
+import random
+
+def encrypt(key: str):
+    encrypted_str = str("")
+
+    if key is None:
+        return TypeError
+
+    string = "%++++++====#$$!$!#@$@#@!"
+    string_array = list(string)
+
+    
+
+    return encrypted_str
