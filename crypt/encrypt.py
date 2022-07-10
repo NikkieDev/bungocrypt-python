d61697de2cd421bb0e4d64d279bb0ac90fb64f53
Nikkie Schaad <Nikkieschaad@gmail.com>
2022-07-10 19:37:32 +0200

    Encryption with key working. Now for the defined string

--- crypt/encrypt.py
+++ crypt/encrypt.py
@@ -1,7 +1,11 @@
 import random
 
-def encrypt(key: str):
+def encrypt(data, key):
+    if len(key) != 42:
+        raise "KeyLengthError: Your key must be 42 characters long!"
+
     encrypted_str = str("")
+    to_encrypt = list(data.lower())
 
     if key is None:
         return TypeError
@@ -9,6 +13,98 @@ def encrypt(key: str):
     string = "%++++++====#$$!$!#@$@#@!"
     string_array = list(string)
 
-    
+    encr_pos = random.randint(0, 24)
 
-    return encrypted_str
+    for char in to_encrypt:
+        if (char == "a"):
+            encrypted_str += key[0]
+        elif (char == "b"):
+            encrypted_str += key[1]
+        elif (char == "c"):
+            encrypted_str += key[2]
+        elif (char == "d"):
+            encrypted_str += key[3]
+        elif (char == "e"):
+            encrypted_str += key[4]
+        elif (char == "f"):
+            encrypted_str += key[5]
+        elif (char == "g"):
+            encrypted_str += key[6]
+        elif (char == "h"):
+            encrypted_str += key[7]
+        elif (char == "i"):
+            encrypted_str += key[8]
+        elif (char == "j"):
+            encrypted_str += key[9]
+        elif (char == "k"):
+            encrypted_str += key[10]
+        elif (char == "l"):
+            encrypted_str += key[11]
+        elif (char == "m"):
+            encrypted_str += key[12]
+        elif (char == "n"):
+            encrypted_str += key[13]
+        elif (char == "o"):
+            encrypted_str += key[14]
+        elif (char == "p"):
+            encrypted_str += key[15]
+        elif (char == "q"):
+            encrypted_str += key[16]
+        elif (char == "r"):
+            encrypted_str += key[17]
+        elif (char == "s"):
+            encrypted_str += key[18]
+        elif (char == "t"):
+            encrypted_str += key[19]
+        elif (char == "u"):
+            encrypted_str += key[20]
+        elif (char == "v"):
+            encrypted_str += key[21]
+        elif (char == "w"):
+            encrypted_str += key[21]
+        elif (char == "x"):
+            encrypted_str += key[23]
+        elif (char == "y"):
+            encrypted_str += key[24]
+        elif (char == "z"):
+            encrypted_str += key[25]
+        elif (char == "1"):
+            encrypted_str += key[26]
+        elif (char == "2"):
+            encrypted_str += key[27]
+        elif (char == "3"):
+            encrypted_str += key[28]
+        elif (char == "4"):
+            encrypted_str += key[29]
+        elif (char == "5"):
+            encrypted_str += key[30]
+        elif (char == "6"):
+            encrypted_str += key[31]
+        elif (char == "7"):
+            encrypted_str += key[32]
+        elif (char == "8"):
+            encrypted_str += key[33]
+        elif (char == "9"):
+            encrypted_str += key[34]
+        elif (char == "0"):
+            encrypted_str += key[35]
+        elif (char == "!"):
+            encrypted_str += key[36]
+        elif (char == "@"):
+            encrypted_str += key[37]
+        elif (char == "#"):
+            encrypted_str += key[38]
+        elif (char == "$"):
+            encrypted_str += key[39]
+        elif (char == "("):
+            encrypted_str += key[40]
+        elif (char == ")"):
+            encrypted_str += key[41]
+        elif (char == "*"):
+            encrypted_str += key[42]
+
+        string_array.insert(encr_pos, encrypted_str)
+
+    return_data = encrypted_str # Returns the encrypted string, and data position in the defined string
+
+    return return_data
--- crypt/key.py
+++ crypt/key.py
@@ -2,11 +2,12 @@ import random
 import string
 
 def create_key():
+    """Only use to store your key. If you want to encrypt your data, please use create_key_arr"""
     key_arr_1 = []
     key_arr_2 = []
     key = ""
 
-    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
     symbols = ["!", "@", "#", "$", "(", ")", "*"]
     total = alphabet + numbers + symbols
@@ -25,3 +26,25 @@ def create_key():
         key += key_char
 
     return key
+
+def create_key_arr():
+    """Creates a 42 character key in an array. This key can be used for encrypting and decrypting. Please store this key somewhere safe (use the arr_to_key function to get a storeable string key.), as you will need it for decrypting your data as well. Losing this key will leave your data encrypted forever, and is unrecoverable!"""
+    key_str = create_key()
+    key_arr = list(key_str)
+
+    return key_arr
+
+def key_to_arr(key: str):
+    """Changes your key from string format to an array that can be used to encrypt and decrypt your data. Key must be 42 characters long"""
+    key_arr = list(key)
+
+    return key_arr
+
+def arr_to_key(key: list):
+    """Changes your key from array to string format that you can store for later use. Key must be 42 characters long"""
+    string = ""
+
+    for key_char in key:
+        string += key_char
+
+    return string
